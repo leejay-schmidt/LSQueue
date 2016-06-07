0bda90fe4da82ef53b1d577b87d8677b2b474496
Leejay Schmidt <leejayschmidt@leejays-macbook-pro.local>
2016-06-07 15:28:19 -0600

    Initial Commit

---
+++ LSQueue/LSQueue.h
@@ -0,0 +1,13 @@
+//
+//  LSQueue.h
+//  LSQueue
+//
+//  Created by Leejay Schmidt on 2016-06-07.
+//  Copyright © 2016 Leejay Schmidt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSQueue : NSObject
+
+@end
---
+++ LSQueue/LSQueue.m
@@ -0,0 +1,13 @@
+//
+//  LSQueue.m
+//  LSQueue
+//
+//  Created by Leejay Schmidt on 2016-06-07.
+//  Copyright © 2016 Leejay Schmidt. All rights reserved.
+//
+
+#import "LSQueue.h"
+
+@implementation LSQueue
+
+@end
